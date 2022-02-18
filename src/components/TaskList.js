17c69a8e5e9016125988be3cb6bf9f10398bb213
Todd Albert <todd@bocacode.com>
2022-02-18 10:57:13 -0500

    getting data from API

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,14 +4,20 @@ import Task from './Task';
 
 const fakeTasks = [
   { id: 1, task: 'Buy Milk', done: false },
-  { id: 2, task: 'Brew Beer', done: false },
+  { id: 2, task: 'Brew Beer', done: true },
   { id: 3, task: 'Buy Trulys', done: false },
-  { id: 4, task: 'Drink Wine', done: false },
+  { id: 4, task: 'Drink Wine', done: true },
   { id: 5, task: 'Buy Paper Towels', done: false },
 ]
 
 export default function TaskList() {
   const [tasks, setTasks] = useState(fakeTasks)
+  useEffect(() => {
+    fetch('https://much-todo-mm.uc.r.appspot.com/tasks')
+      .then(response => response.json())
+      .then(data => setTasks(data))
+      .catch(alert)
+  }, [])
   return (
     <List
       bordered
