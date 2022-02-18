c5e531f44070de2ea495c5c617c884ade4750095
jonathans199 <jso1919@gmail.com>
2022-02-18 15:49:21 -0500

    posting new tasks to API

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react"
-import { Input } from "antd";
-
+import { Input } from 'antd'
+import { useState } from 'react'
 
 export default function NewTask() {
-  const [task, setTask] = useState("");
-  
+  const [newTask, setNewTask] = useState('')
+
+  const taskObject = {
+    task: newTask,
+  }
+
+  const handleButtonSubmit = () => {
+    console.log('sending to API')
+
+    fetch('https://much-todo-bc.uc.r.appspot.com/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(taskObject),
+    })
+      .then(response => response.json())
+      .then(data => console.log('data was added', data))
+      .catch(err => console.error(err))
+  }
+
+  const handleInputText = event => {
+    setNewTask(event.target.value)
+  }
+
+  console.log('newTask state here ->>', newTask)
+
   return (
     <>
-      <Input placeholder="Enter task name" />
+      <h2>Add new task</h2>
+      <Input placeholder='Enter task name' onChange={event => handleInputText(event)} />
+
+      <button onClick={handleButtonSubmit}>Send new task to api</button>
     </>
-  );
+  )
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,30 +1,43 @@
-import { useState, useEffect } from 'react';
-import { List } from 'antd';
-import Task from './Task';
+import { useState, useEffect } from 'react'
+import { List } from 'antd'
+import Task from './Task'
 
-const fakeTasks = [
-  { id: 1, task: 'Buy Milk', done: false },
-  { id: 2, task: 'Brew Beer', done: true },
-  { id: 3, task: 'Buy Trulys', done: false },
-  { id: 4, task: 'Drink Wine', done: true },
-  { id: 5, task: 'Buy Paper Towels', done: false },
-]
+// const fakeTasks = [
+//   { id: 1, task: 'Buy Milk', done: false },
+//   { id: 2, task: 'Brew Beer', done: true },
+//   { id: 3, task: 'Buy Trulys', done: false },
+//   { id: 4, task: 'Drink Wine', done: true },
+//   { id: 5, task: 'Buy Paper Towels', done: false },
+//   { id: 6, task: 'Buy beer', done: false },
+// ]
 
 export default function TaskList() {
-  const [tasks, setTasks] = useState(fakeTasks)
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
+    // GET DATA FROM API
     fetch('https://much-todo-bc.uc.r.appspot.com/tasks')
-    .then(response => response.json())
-    .then(data => setTasks(data))
-    .catch(alert)
-},[])
+      .then(response => response.json())
+      .then(data => setTasks(data))
+      .catch(alert)
+  }, [])
+
+  // tasks has all the data
+
+  // we need to pass this TASKS => data to the List component
+  // return <List bordered dataSource={tasks} renderItem={item => <Task item={item} />} />
 
   return (
-    <List
-      bordered
-      dataSource={tasks}
-      renderItem={item => <Task item={item} />}
-    />
+    <>
+      <ul>
+        {tasks &&
+          tasks.map(singleTask => {
+            return <li key={singleTask.id}>{singleTask.task}</li>
+          })}
+
+        <li>item 2 </li>
+        <li>item 3</li>
+      </ul>
+    </>
   )
 }
