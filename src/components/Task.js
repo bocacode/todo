db6821fc12f8779130b3617685d7e12145a2e511
Darie Dorlus <tdorluss@gmail.com>
2022-02-21 16:57:15 -0500

    Added a checkbox toggle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,31 +1,38 @@
-import { useState, useEffect } from 'react';
-import { List } from 'antd';
+import { useState, useEffect } from "react";
+import { List, Checkbox } from "antd";
 
 export default function Task({ item, setTasks }) {
-  const [itemStyle, setItemStyle] = useState({})
+  const [itemStyle, setItemStyle] = useState({});
   useEffect(() => {
-    if(item.done) {
-      setItemStyle({ color: 'grey', textDecoration: 'line-through' });
+    if (item.done) {
+      setItemStyle({ color: "grey", textDecoration: "line-through" });
     } else {
-      setItemStyle({ color: 'black', textDecoration: 'none' });
+      setItemStyle({ color: "black", textDecoration: "none" });
     }
-  }, [item])
+  }, [item]);
   const handleToggleTaskDone = () => {
     // call api -- patch: `/tasks/${item.id}` send { done: !item.done }
     fetch(`https://much-todo-bc.uc.r.appspot.com/tasks/${item.id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ done: !item.done })
+      body: JSON.stringify({ done: !item.done }),
     })
       .then(() => {
         // THEN: fetch our tasks
-        fetch('https://much-todo-bc.uc.r.appspot.com/tasks')
-          .then(response => response.json())
-          .then(data => setTasks(data))
+        fetch("https://much-todo-bc.uc.r.appspot.com/tasks")
+          .then((response) => response.json())
+          .then((data) => setTasks(data));
       })
-      .catch(alert)
-  }
-  return <List.Item onClick={handleToggleTaskDone} style={itemStyle}>{item.task}</List.Item>
+      .catch(alert);
+  };
+  return (
+    <>
+      <List.Item style={itemStyle}>
+        <Checkbox style={{margin: "10px"}} onClick={handleToggleTaskDone} checked={item.done}></Checkbox>
+        {item.task}
+      </List.Item>
+    </>
+  );
 }
